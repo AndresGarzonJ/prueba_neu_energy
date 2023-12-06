607e07dac9bfc6d051593ededc2b78e5621e8f5c
Andres Garzon <sebasgarzonj@gmail.com>
2023-12-06 14:23:06 -0500

    Update main.py

--- src/python/main.py
+++ src/python/main.py
@@ -5,6 +5,8 @@ from contoller.energy_bill import calculate_energy_bill
 year = 2023
 month = 9
 result = calculate_energy_bill(session, month, year)
-print(result)
+
+for id_service, EA, EC, EE1, EE2 in result:
+    print("id_service: {:.2f} EA: {:.2f} EC: {:.2f} EE1: {:.2f} EE2: {:.2f}".format(id_service, EA, EC, EE1, EE2))
 
 session.close()
